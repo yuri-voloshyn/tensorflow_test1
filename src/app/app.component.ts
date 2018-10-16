66c93f24ed6f740d1f5fb48629efd807dd6d52b8
Sergiy <sergey.voloshyn@gmail.com>
2018-10-16 16:11:08 +0300

    RNN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,17 @@
 import { Component } from '@angular/core';
-import {
-  Sequential,
-  sequential,
-  layers,
-  tensor2d,
-  loadModel,
-  Tensor
-} from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
+import { CharacterTable } from './character-table';
+import { generateData, convertDataToTensors } from './utils';
 
-const data = {
-  inputs: [[0, 0], [0, 1], [1, 0], [1, 1]],
-  targets: [[0], [1], [1], [0]]
-};
-
-const hiddenLayer = layers.dense({
-  units: 10,
-  inputShape: [2],
-  activation: 'sigmoid'
-});
-const outputLayer = layers.dense({
-  units: 1,
-  inputShape: [10],
-  activation: 'sigmoid'
-});
+const CHARS = '0123456789+ ';
+const DIGITS = 2;
+const TRAINING_SIZE = 5000;
+const RNN_TYPE = 'SimpleRNN';
+const RNN_LAYERS = 1;
+const RNN_LAYER_SIZE = 128;
+const BATCH_SIZE = 128;
+const TRAIN_ITERATIONS = 100;
+const COUNT_OF_TESTS = 20;
 
 @Component({
   selector: 'app-root',
@@ -30,40 +19,207 @@ const outputLayer = layers.dense({
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  model: Sequential;
-
+  charTable: CharacterTable;
+  model: tf.Sequential;
+  trainData;
+  testData;
+  trainXs;
+  trainYs;
+  testXs;
+  testYs;
+  testXsForDisplay;
   logMessage: string;
 
-  constructor() {}
+  lossValues = [];
+  accuracyValues = [];
+  examplesPerSecValues = [];
+
+  displayData: { text: string; correct: boolean }[] = [];
+
+  constructor() {
+    this.charTable = new CharacterTable(CHARS);
+    const data = generateData(DIGITS, TRAINING_SIZE, false);
+    const split = Math.floor(TRAINING_SIZE * 0.9);
+    this.trainData = data.slice(0, split);
+    this.testData = data.slice(split);
+    [this.trainXs, this.trainYs] = convertDataToTensors(
+      this.trainData,
+      this.charTable,
+      DIGITS
+    );
+    [this.testXs, this.testYs] = convertDataToTensors(
+      this.testData,
+      this.charTable,
+      DIGITS
+    );
+    this.model = this.createAndCompileModel(
+      RNN_LAYERS,
+      RNN_LAYER_SIZE,
+      RNN_TYPE,
+      DIGITS,
+      CHARS.length
+    );
+  }
 
   async train() {
-    // Define a model for linear regression.
-    this.model = sequential();
-    this.model.add(hiddenLayer);
-    this.model.add(outputLayer);
+    this.lossValues = [];
+    this.accuracyValues = [];
+    this.examplesPerSecValues = [];
 
-    // Prepare the model for training: Specify the loss and the optimizer.
-    this.model.compile({ loss: 'meanSquaredError', optimizer: 'rmsprop' });
+    for (let i = 0; i < TRAIN_ITERATIONS; ++i) {
+      const beginMs = performance.now();
+      const history = await this.model.fit(this.trainXs, this.trainYs, {
+        epochs: 1,
+        batchSize: BATCH_SIZE,
+        validationData: [this.testXs, this.testYs],
+        yieldEvery: 'epoch'
+      });
+      const elapsedMs = performance.now() - beginMs;
+      const examplesPerSec = this.testXs.shape[0] / (elapsedMs / 1000);
+      const trainLoss = history.history['loss'][0] as number;
+      const trainAccuracy = history.history['acc'][0] as number;
+      const valLoss = history.history['val_loss'][0] as number;
+      const valAccuracy = history.history['val_acc'][0] as number;
 
-    // Generate some synthetic data for training.
-    const xs = tensor2d(data.inputs);
-    const ys = tensor2d(data.targets);
+      this.log(
+        `Iteration ${i}: train loss = ${trainLoss.toFixed(6)}; ` +
+          `train accuracy = ${trainAccuracy.toFixed(6)}; ` +
+          `validation loss = ${valLoss.toFixed(6)}; ` +
+          `validation accuracy = ${valAccuracy.toFixed(6)} ` +
+          `(${examplesPerSec.toFixed(1)} examples/s)`
+      );
 
-    for (let i = 1; i < 300; ++i) {
-      const h = await this.model.fit(xs, ys, { epochs: 10 });
-      this.log(`Loss after Epoch ${i} : ${h.history.loss[0]}`);
+      this.lossValues.push({ epoch: i, loss: trainLoss, set: 'train' });
+      this.lossValues.push({ epoch: i, loss: valLoss, set: 'validation' });
+      this.accuracyValues.push({
+        epoch: i,
+        accuracy: trainAccuracy,
+        set: 'train'
+      });
+      this.accuracyValues.push({
+        epoch: i,
+        accuracy: valAccuracy,
+        set: 'validation'
+      });
+      this.examplesPerSecValues.push({
+        epoch: i,
+        'examples/s': examplesPerSec
+      });
     }
   }
 
   test() {
-    this.log(String(this.predict(0, 1)));
-  }
+    if (!this.testXsForDisplay) {
+      this.testXsForDisplay = this.testXs.slice(
+        [0, 0, 0],
+        [COUNT_OF_TESTS, this.testXs.shape[1], this.testXs.shape[2]]
+      );
+    }
 
-  predict(x: number, y: number): number {
-    return (this.model.predict(tensor2d([[x, y]])) as Tensor).dataSync()[0];
+    this.displayData = [];
+    tf.tidy(() => {
+      const predictOut = this.model.predict(this.testXsForDisplay) as tf.Tensor;
+      for (let k = 0; k < COUNT_OF_TESTS; ++k) {
+        const scores = predictOut
+          .slice([k, 0, 0], [1, predictOut.shape[1], predictOut.shape[2]])
+          .as2D(predictOut.shape[1], predictOut.shape[2]);
+        const decoded = this.charTable.decode(scores);
+        this.displayData.push({
+          text: this.testData[k][0] + ' = ' + decoded,
+          correct: this.testData[k][1].trim() === decoded.trim()
+        });
+      }
+    });
   }
 
   log(message: string) {
     this.logMessage = message;
+  }
+
+  createAndCompileModel(
+    layers: number,
+    hiddenSize: number,
+    rnnType: 'SimpleRNN' | 'GRU' | 'LSTM',
+    digits: number,
+    vocabularySize: number
+  ) {
+    const maxLen = digits + 1 + digits;
+
+    const model = tf.sequential();
+    switch (rnnType) {
+      case 'SimpleRNN':
+        model.add(
+          tf.layers.simpleRNN({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            inputShape: [maxLen, vocabularySize]
+          })
+        );
+        break;
+      case 'GRU':
+        model.add(
+          tf.layers.gru({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            inputShape: [maxLen, vocabularySize]
+          })
+        );
+        break;
+      case 'LSTM':
+        model.add(
+          tf.layers.lstm({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            inputShape: [maxLen, vocabularySize]
+          })
+        );
+        break;
+      default:
+        throw new Error(`Unsupported RNN type: '${rnnType}'`);
+    }
+    model.add(tf.layers.repeatVector({ n: digits + 1 }));
+    switch (rnnType) {
+      case 'SimpleRNN':
+        model.add(
+          tf.layers.simpleRNN({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            returnSequences: true
+          })
+        );
+        break;
+      case 'GRU':
+        model.add(
+          tf.layers.gru({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            returnSequences: true
+          })
+        );
+        break;
+      case 'LSTM':
+        model.add(
+          tf.layers.lstm({
+            units: hiddenSize,
+            recurrentInitializer: 'glorotNormal',
+            returnSequences: true
+          })
+        );
+        break;
+      default:
+        throw new Error(`Unsupported RNN type: '${rnnType}'`);
+    }
+    model.add(
+      tf.layers.timeDistributed({
+        layer: tf.layers.dense({ units: vocabularySize })
+      })
+    );
+    model.add(tf.layers.activation({ activation: 'softmax' }));
+    model.compile({
+      loss: 'categoricalCrossentropy',
+      optimizer: 'adam',
+      metrics: ['accuracy']
+    });
+    return model;
   }
 }
---
+++ src/app/utils.ts
@@ -0,0 +1,62 @@
+/**
+ * Generate examples.
+ *
+ * Each example consists of a question, e.g., '123+456' and and an
+ * answer, e.g., '579'.
+ *
+ * @param digits Maximum number of digits of each operand of the
+ * @param numExamples Number of examples to generate.
+ * @param invert Whether to invert the strings in the question.
+ * @returns The generated examples.
+ */
+export function generateData(digits, numExamples, invert) {
+  const digitArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const arraySize = digitArray.length;
+
+  const output = [];
+  const maxLen = digits + 1 + digits;
+
+  const f = () => {
+    let str = '';
+    while (str.length < digits) {
+      const index = Math.floor(Math.random() * arraySize);
+      str += digitArray[index];
+    }
+    return Number.parseInt(str);
+  };
+
+  const seen = new Set();
+  while (output.length < numExamples) {
+    const a = f();
+    const b = f();
+    const sorted = b > a ? [a, b] : [b, a];
+    const key = sorted[0] + '`' + sorted[1];
+    if (seen.has(key)) {
+      continue;
+    }
+    seen.add(key);
+
+    // Pad the data with spaces such that it is always maxLen.
+    const q = `${a}+${b}`;
+    const query = q + ' '.repeat(maxLen - q.length);
+    let ans = (a + b).toString();
+    // Answer can be of maximum size `digits + 1`.
+    ans += ' '.repeat(digits + 1 - ans.length);
+
+    if (invert) {
+      throw new Error('invert is not implemented yet');
+    }
+    output.push([query, ans]);
+  }
+  return output;
+}
+
+export function convertDataToTensors(data, charTable, digits) {
+  const maxLen = digits + 1 + digits;
+  const questions = data.map(datum => datum[0]);
+  const answers = data.map(datum => datum[1]);
+  return [
+    charTable.encodeBatch(questions, maxLen),
+    charTable.encodeBatch(answers, digits + 1)
+  ];
+}
